951140019f5c7af5575e2bc2ac8b0a1a861bd2da
Jonattan <jonattan.sousa@gmail.com>
2021-05-24 14:56:18 -0300

    Primeiro commit, projeto em branco.

---
+++ AppNoticias/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppNoticias
+//
+//  Created by Jonattan Moises Sousa on 24/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
